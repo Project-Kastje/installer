492508adc669100ef27c1e999ca7791dde466fe9
hypothermic <admin@hypothermic.nl>
2019-09-09 14:08:39 +0200

    Start de service

--- install.sh
+++ install.sh
@@ -8,9 +8,18 @@ rm -rf /etc/project-kastje/
 useradd -M -N -r -s /bin/false -c "Project Kastje System User" pkuser
 mkdir -p /etc/project-kastje/
 
+
+
 # Clone alle services
 git clone https://github.com/Project-Kastje/backend-service /etc/project-kastje/backend-service/
 chmod +x /etc/project-kastje/backend-service/service.py
 
 
+
+# Start alle services
+screen -d -m -S backend-service-screen bash -c 'python3 /etc/project-kastje/backend-service/service.py'
+
+
+
+# Root toegang is nodig om de systeembestanden te zien
 chmod -R 000 /etc/project-kastje/
