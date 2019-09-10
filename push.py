13486c3bdadb5f4e2f451d5a863a2f44e4648c84
Job Vulkers <job.vulkers@gmail.com>
2019-09-10 11:48:09 +0200

    Push.py verwijderd

--- push.py
+++
@@ -1,7 +0,0 @@
-from pushbullet import Pushbullet
-
-pb = Pushbullet("o.N9fN9fxDfWLsjqaChdWJ2SmFRbgIrY0i")
-print(pb.devices)
-
-dev = pb.get_device("Galaxy S9 Plus")
-push = dev.push_note("Waarschuwing!!","Je kastje is open gegaan")
