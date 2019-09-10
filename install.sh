785ab002f6d8080bb48206f923fb536fc9bef6f1
hypothermic <admin@hypothermic.nl>
2019-09-10 12:19:49 +0200

    Config apache2 en enable python cgi

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ mkdir -p /etc/project-kastje/
 
 # Installeer dependencies met aptitude
 apt-get update && apt-get upgrade
-apt-get --assume-yes install screen build-dep python3 python3-pip pygame
+apt-get --assume-yes install screen python3 python3-pip
 
 # Kill alle vorige screen instances
 screen -ls | awk -vFS='\t|[.]' '/backend-service-screen/ {system("screen -S "$2" -X quit")}'
@@ -29,12 +29,19 @@ mariadb -u root < /etc/project-kastje/backend-service/create-database.sql
 pip install pushbullet.py
 pip install pygame
 
-# Start alle services
+# Start backend-service en check of screen niet gecrashed is
 screen -d -m -S backend-service-screen bash -c 'python3 /etc/project-kastje/backend-service/service.py'
+sudo screen -list 2>&1 | tee installer.log
 
+# Installeer apache2
+apt-get purge apache2
+apt-get install apache2
+#service apache2 status
+a2enmod cgi
+mv ./frontend-website-apache.conf /etc/apache2/sites-enabled/000-default.conf
+service apache2 restart
 
-
-# Root toegang is nodig om de systeembestanden te zien
+# Root toegang zal nodig zijn om de systeembestanden te zien
 chmod -R 000 /etc/project-kastje/
 
 echo ""
